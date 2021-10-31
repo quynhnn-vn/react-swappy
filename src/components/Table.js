ce0e4d87f8941d7e307eadd360d6411012c1a218
quynhnn-vn <nnquynh.0111@gmail.com>
2021-10-31 14:36:07 +0100

    Update styles for table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/Table.css";
 import { useSelector } from "react-redux";
-import { formatDateForHeader } from "../utils";
+import { formatDate, formatDateForHeader } from "../utils";
 import Service from "./Service";
 import SwitchWeek from "./SwitchWeek";
 
@@ -9,22 +9,40 @@ export default function Table() {
     const week = useSelector((state) => state.week.week);
     const services = useSelector((state) => state.services.services);
 
-    const renderHeader = () =>
+    const renderDate = () =>
         week.dates &&
         ["CRÉNEAUX", ...week.dates].map((date, index) => (
-            <th key={index}>
+            <th
+                key={index}
+                className={
+                    date === formatDate(new Date()) ? "current-date" : null
+                }
+            >
+                {console.log(date === formatDate(new Date()))}
                 {date !== "CRÉNEAUX"
                     ? formatDateForHeader(new Date(date))
                     : date}
             </th>
         ));
+    const renderSlot = () => {
+        week.dates &&
+            ["CRÉNEAUX", ...week.dates].map((date, index) => (
+                <th key={index}>
+                    {date !== "CRÉNEAUX"
+                        ? formatDateForHeader(new Date(date))
+                        : date}
+                </th>
+            ));
+    };
     return (
         <div className="table-container">
             <SwitchWeek />
-
             <table>
-                <thead className="table-header">{renderHeader()}</thead>
+                <thead className="table-header">
+                    <tr>{renderDate()}</tr>
+                </thead>
                 <tbody className="table-body">
+                    <tr>{renderSlot()}</tr>
                     {services.map((service) => (
                         <tr key={service.id}>
                             <Service service={service} />
